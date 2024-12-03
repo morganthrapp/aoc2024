1e00b0af921763060c363ecd63a6330e3a576868
Morgan Thrapp <mpthrapp@gmail.com>
2024-12-03 11:37:56 -0800

    No need to read the file twice

--- python/main.py
+++ python/main.py
@@ -10,13 +10,15 @@ for day in range(1, datetime.datetime.now().day + 1):
     print(f'Day {day} has not been implemented yet')
     print(e)
     continue
+
+  data = get_input(day)
   
-  print(f'Day {day} - Part 1: {module.part_one(get_input(day))}')
+  print(f'Day {day} - Part 1: {module.part_one(data)}')
   try:
     part_two = module.part_two
   except AttributeError:
     print(f'Day {day} Part 2 not implemented yet')
     continue
 
-  print(f'Day {day} - Part 2: {module.part_two(get_input(day))}')
+  print(f'Day {day} - Part 2: {module.part_two(data)}')
   
