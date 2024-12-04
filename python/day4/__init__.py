419fb8ea6a6faa1bd9f7945328ca7d2cceaf3a15
Morgan Thrapp <mpthrapp@gmail.com>
2024-12-04 09:57:13 -0800

    This might be useful for another day

--- python/day4/__init__.py
+++ python/day4/__init__.py
@@ -1,14 +1,9 @@
 import itertools
 
+from utils.aoc_utils import get_item
+
 
 def part_one(data):
-    def get_item(data, x, y, x_adjustment, y_adjustment, column_count, row_count):
-        adjusted_x = x + x_adjustment
-        adjusted_y = y + y_adjustment
-        if not (0 <= adjusted_x <= column_count) or not (0 <= adjusted_y <= row_count):
-            raise IndexError()
-        return data[adjusted_y][adjusted_x]
-
     data = list(map(lambda r: r.strip(), data))
     xmas_coords = set()
     x = 0
@@ -19,44 +14,22 @@ def part_one(data):
         if data[y][x] == "X":
             for x_adjustment, y_adjustment in itertools.product([-1, 0, 1], repeat=2):
                 try:
-                    if (
-                        get_item(
-                            data,
-                            x,
-                            y,
-                            x_adjustment,
-                            y_adjustment,
-                            column_count,
-                            row_count,
-                        )
-                        == "M"
-                    ):
-                        if (
+                    for i, l in enumerate("MAS", 1):
+                        if not (
                             get_item(
                                 data,
                                 x,
                                 y,
-                                x_adjustment * 2,
-                                y_adjustment * 2,
+                                x_adjustment * i,
+                                y_adjustment * i,
                                 column_count,
                                 row_count,
                             )
-                            == "A"
+                            == l
                         ):
-                            if (
-                                get_item(
-                                    data,
-                                    x,
-                                    y,
-                                    x_adjustment * 3,
-                                    y_adjustment * 3,
-                                    column_count,
-                                    row_count,
-                                )
-                                == "S"
-                            ):
-                                xmas_coords.add(f"{y},{x}-{y_adjustment,x_adjustment}")
-                except IndexError:
+                            raise IndexError()
+                    xmas_coords.add(f"{y},{x}-{y_adjustment,x_adjustment}")
+                except:
                     pass
         if x < column_count:
             x += 1
@@ -68,13 +41,6 @@ def part_one(data):
 
 
 def part_two(data):
-    def get_item(data, x, y, x_adjustment, y_adjustment, column_count, row_count):
-        adjusted_x = x + x_adjustment
-        adjusted_y = y + y_adjustment
-        if not (0 <= adjusted_x <= column_count) or not (0 <= adjusted_y <= row_count):
-            raise IndexError()
-        return data[adjusted_y][adjusted_x]
-
     data = list(map(lambda r: r.strip(), data))
     xmas_coords = set()
     x = 0
@@ -84,19 +50,37 @@ def part_two(data):
     while y <= row_count:
         if data[y][x] == "A":
             try:
-                if sum([(
-                    get_item(data, x, y, -1, -1, column_count, row_count) == "M"
-                    and get_item(data, x, y, 1, 1, column_count, row_count) == "S"
-                ) , (
-                    get_item(data, x, y, 1, -1, column_count, row_count) == "S"
-                    and get_item(data, x, y, -1, 1, column_count, row_count) == "M"
-                ) , (
-                    get_item(data, x, y, -1, -1, column_count, row_count) == "S"
-                    and get_item(data, x, y, 1, 1, column_count, row_count) == "M"
-                ) , (
-                    get_item(data, x, y, 1, -1, column_count, row_count) == "M"
-                    and get_item(data, x, y, -1, 1, column_count, row_count) == "S"
-                )]) == 2:
+                if (
+                    sum(
+                        [
+                            (
+                                get_item(data, x, y, -1, -1, column_count, row_count)
+                                == "M"
+                                and get_item(data, x, y, 1, 1, column_count, row_count)
+                                == "S"
+                            ),
+                            (
+                                get_item(data, x, y, 1, -1, column_count, row_count)
+                                == "S"
+                                and get_item(data, x, y, -1, 1, column_count, row_count)
+                                == "M"
+                            ),
+                            (
+                                get_item(data, x, y, -1, -1, column_count, row_count)
+                                == "S"
+                                and get_item(data, x, y, 1, 1, column_count, row_count)
+                                == "M"
+                            ),
+                            (
+                                get_item(data, x, y, 1, -1, column_count, row_count)
+                                == "M"
+                                and get_item(data, x, y, -1, 1, column_count, row_count)
+                                == "S"
+                            ),
+                        ]
+                    )
+                    == 2
+                ):
 
                     xmas_coords.add(f"{y},{x}")
             except IndexError:
--- python/utils/aoc_utils.py
+++ python/utils/aoc_utils.py
@@ -1,10 +1,20 @@
 from collections.abc import Iterator
 
+
 def get_input(day: int) -> list[str]:
-  with open(f'inputs/day{day}', 'r') as input_data:
-    return input_data.readlines()
+    with open(f"inputs/day{day}", "r") as input_data:
+        return input_data.readlines()
+
 
 def stream_input(day: int) -> Iterator[str]:
-  with open(f'inputs/day{day}', 'r') as input_data:
-    while line := input_data.readline():
-      yield line
+    with open(f"inputs/day{day}", "r") as input_data:
+        while line := input_data.readline():
+            yield line
+
+
+def get_item(data, x, y, x_adjustment, y_adjustment, column_count, row_count):
+    adjusted_x = x + x_adjustment
+    adjusted_y = y + y_adjustment
+    if not (0 <= adjusted_x <= column_count) or not (0 <= adjusted_y <= row_count):
+        raise IndexError()
+    return data[adjusted_y][adjusted_x]
